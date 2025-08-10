25b17fc3b562c7a0febe4b1b7492cb6a68f30dfb
hangsheng0625 <hangsheng279@gmail.com>
2025-08-10 13:42:51 +0800

    Clean up old files and finalize project structure

--- 3D-Portfolio/src/main.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client"; // Correct import
-import App from "./App";
-import "./index.css";
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
